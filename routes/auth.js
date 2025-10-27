7d3909c92715b1f2af1333cfa3a0d8cfb442983d
Imadnan23 <adnanadil93045@gmail.com>
2025-10-27 17:10:28 +0530

    fixed auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/auth');
 
 router.post('/login', authController.login);
 router.post('/create-user', authController.createUser); // Use once, then remove
-router.post('/change-password', authController.changePassword);
-
+router.post('/change-password', authMiddleware, authController.changePassword);
 
 module.exports = router;

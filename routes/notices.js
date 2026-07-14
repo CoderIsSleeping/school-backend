5ab3cd5a8c4e8b8d754ea1b340861ce50c0cf622
CoderIsSleeping <adnanadil93045@gmail.com>
2026-07-15 01:09:20 +0530

    Update notices.js

--- routes/notices.js
+++ routes/notices.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const noticeController = require('../controllers/noticeController');
 const authMiddleware = require('../middleware/auth');
+const { allowRoles } = require('../middleware/roles');
 
-router.post('/create', authMiddleware, noticeController.createNotice);
+router.post('/create', authMiddleware, allowRoles('admin'),noticeController.createNotice);
 router.get('/all', noticeController.getAllNotices);
 router.get('/latest', noticeController.getLatestNotice);
-router.delete('/:id', authMiddleware, noticeController.deleteNotice);
+router.delete('/:id', authMiddleware, allowRoles('admin'),noticeController.deleteNotice);
 
 module.exports = router;

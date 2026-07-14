81a7ae1272a401619d8a2b7e8d1c6fdc340efbc1
CoderIsSleeping <adnanadil93045@gmail.com>
2026-07-15 01:10:17 +0530

    Update images.js

--- routes/images.js
+++ routes/images.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const imageController = require('../controllers/imageController');
 const authMiddleware = require('../middleware/auth');
+const { allowRoles } = require('../middleware/roles');
 
-router.post('/upload', authMiddleware, imageController.uploadImage);
+router.post('/upload', authMiddleware,allowRoles('admin','teacher'), imageController.uploadImage);
 router.get('/all', imageController.getAllImages);
-router.delete('/:id', authMiddleware, imageController.deleteImage);
+router.delete('/:id', authMiddleware,allowRoles('admin'), imageController.deleteImage);
 
 module.exports = router;
